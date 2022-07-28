fc0e0f719273e23e134c8f040ff206eca0fc599a
Josa Vicente <jvicenpe@gmail.com>
2022-07-28 21:27:19 +0200

    Initial Commit

---
+++ CarrouselParallax/CarrouselParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrouselParallaxApp.swift
+//  CarrouselParallax
+//
+//  Created by Josafat Vicente Pérez on 28/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CarrouselParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
